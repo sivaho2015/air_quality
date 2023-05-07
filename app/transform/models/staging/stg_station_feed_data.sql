c4d1e5d0959998913a9bba45a2da9f31a5df1adc
sivaho2015 <sivaho2015@gmail.com>
2023-05-07 14:57:54 -0500

    reformat stg_station_feed_data.sql

--- app/transform/models/staging/stg_station_feed_data.sql
+++ app/transform/models/staging/stg_station_feed_data.sql
@@ -1,54 +1,54 @@
-with source as (
-    select *
-    from {{ source('air_quality', 'station_feed') }}
+WITH source AS (
+    SELECT *
+    FROM {{ source('air_quality', 'station_feed') }}
 )
-select
-    station_id::integer,
+SELECT
+    station_id::INTEGER,
     city_name,
-    case
-        when latitude <> 'Unknown' then latitude::numeric
-    end as latitude,
-    case
-        when longitude <> 'Unknown' then longitude::numeric
-    end as longitude,
-    case
-        when local_measurement_time <> 'Unknown' then local_measurement_time::timestamp
-    end as local_measurement_time,
-    case
-        when local_measurement_time <> 'Unknown' then local_measurement_time
-    end as local_measurement_timezone,
-    case 
-        when local_measurement_timezone = '-07:00' then 'PST'
-        when local_measurement_timezone = '-06:00' then 'MST'
-        when local_measurement_timezone = '-05:00' then 'CST'
-        when local_measurement_timezone = '-04:00' then 'EST'
-        when local_measurement_timezone = '-03:00' then 'AST'
-    end as local_measurement_timezone_text,
-    case
-        when carbon_monoxide <> 'Unknown' then carbon_monoxide::numeric
-    end as carbon_monoxide,
-    case
-        when humidity <> 'Unknown' then humidity::numeric
-    end as humidity,
-    case
-        when ozone <> 'Unknown' then ozone::numeric
-    end as ozone,
-    case
-        when pressure <> 'Unknown' then pressure::numeric
-    end as pressure,
-    case
-        when pm_10 <> 'Unknown' then pm_10::numeric
-    end as pm_10,
-    case
-        when pm_2_5 <> 'Unknown' then pm_2_5::numeric
-    end as pm_2_5,
-    case
-        when sulfur_dioxide <> 'Unknown' then sulfur_dioxide::numeric
-    end as sulfur_dioxide,
-    case
-        when temperature <> 'Unknown' then temperature::numeric
-    end as temperature,
-    case
-        when wind_speed <> 'Unknown' then wind_speed::numeric
-    end as wind_speed
-from source
+    CASE
+        WHEN latitude <> 'Unknown' THEN latitude::NUMERIC
+    END AS latitude,
+    CASE
+        WHEN longitude <> 'Unknown' THEN longitude::NUMERIC
+    END AS longitude,
+    CASE
+        WHEN local_measurement_time <> 'Unknown' THEN local_measurement_time::TIMESTAMP
+    END AS local_measurement_time,
+    CASE
+        WHEN local_measurement_time <> 'Unknown' THEN local_measurement_time
+    END AS local_measurement_timezone,
+    CASE 
+        WHEN local_measurement_timezone = '-07:00' THEN 'PST'
+        WHEN local_measurement_timezone = '-06:00' THEN 'MST'
+        WHEN local_measurement_timezone = '-05:00' THEN 'CST'
+        WHEN local_measurement_timezone = '-04:00' THEN 'EST'
+        WHEN local_measurement_timezone = '-03:00' THEN 'AST'
+    END AS local_measurement_timezone_text,
+    CASE
+        WHEN carbon_monoxide <> 'Unknown' THEN carbon_monoxide::NUMERIC
+    END AS carbon_monoxide,
+    CASE
+        WHEN humidity <> 'Unknown' THEN humidity::NUMERIC
+    END AS humidity,
+    CASE
+        WHEN ozone <> 'Unknown' THEN ozone::NUMERIC
+    END AS ozone,
+    CASE
+        WHEN pressure <> 'Unknown' THEN pressure::NUMERIC
+    END AS pressure,
+    CASE
+        WHEN pm_10 <> 'Unknown' THEN pm_10::NUMERIC
+    END AS pm_10,
+    CASE
+        WHEN pm_2_5 <> 'Unknown' THEN pm_2_5::NUMERIC
+    END AS pm_2_5,
+    CASE
+        WHEN sulfur_dioxide <> 'Unknown' THEN sulfur_dioxide::NUMERIC
+    END AS sulfur_dioxide,
+    CASE
+        WHEN temperature <> 'Unknown' THEN temperature::NUMERIC
+    END AS temperature,
+    CASE
+        WHEN wind_speed <> 'Unknown' THEN wind_speed::NUMERIC
+    END AS wind_speed
+FROM source
