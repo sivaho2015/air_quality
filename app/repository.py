ac0e20f99695c8eb3ff0eb8517f0bbe83aec469a
sivaho2015 <sivaho2015@gmail.com>
2023-05-07 15:14:43 -0500

    add comments

--- app/repository.py
+++ app/repository.py
@@ -5,7 +5,7 @@ from dagster import repository
 from jobs.data_ingestion import data_ingestion
 from schedules.data_ingestion_schedule import data_ingestion_schedule
 
-
+# Deploy Dagster repository
 @repository
 def deploy_docker_repository():
     return [data_ingestion, data_ingestion_schedule]
